ce04520b15fafb51d7cf8bfb89665a5a5a58ffb3
Shin Han Seo <imkara1@naver.com>
2026-06-19 15:06:44 +0900

    Feat : MyPageApi 변경에 따른 코드 수정(레파지토리 메서드 추가)

--- app/src/main/java/com/umc/hackathon/frontend/feature/mypage/data/repository/MyPageRepository.kt
+++ app/src/main/java/com/umc/hackathon/frontend/feature/mypage/data/repository/MyPageRepository.kt
@@ -1,8 +1,17 @@
 package com.umc.hackathon.frontend.feature.mypage.data.repository
 
+import com.umc.hackathon.frontend.feature.mypage.model.MyDistrict
 import com.umc.hackathon.frontend.feature.mypage.model.UserProfile
 
 interface MyPageRepository {
     suspend fun getMyProfile(): UserProfile?
+
+    suspend fun updateNickname(nickname: String): UserProfile?
+
+    suspend fun getCurrentDistrict(
+        latitude: Double,
+        longitude: Double
+    ): MyDistrict?
+
     suspend fun logout()
 }
